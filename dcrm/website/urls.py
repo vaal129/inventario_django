e0a129e469895599f292f1022a20030ec4233a3d
vaal129 <valeriacuestavcc@gmail.com>
2026-06-22 16:04:13 -0500

    Commit 17: Enrutamiento de URLs

---
+++ dcrm/website/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('register/', views.register_user, name='register'),
+    path('logout/', views.logout_user, name='logout'),
+    path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
+    path('dashboard/', views.passenger_dashboard, name='passenger_dashboard'),
+    path('toggle-user/<int:user_id>/', views.toggle_user_status, name='toggle_user_status'),
+    path('edit-user/<int:user_id>/', views.edit_user, name='edit_user'),
+    path('api/reports/', views.api_reports, name='api_reports'),
+    path('api/notifications/', views.api_notifications, name='api_notifications'),
+]
