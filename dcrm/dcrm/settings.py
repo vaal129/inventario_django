567d3f726711ac935cb427e0e091d34c93136389
vaal129 <valeriacuestavcc@gmail.com>
2026-06-23 00:59:51 -0500

    Refactor: Mover carpeta static al mismo nivel de templates según buenas prácticas de Django

--- dcrm/dcrm/settings.py
+++ dcrm/dcrm/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'website', 'templates', 'static'),
+    os.path.join(BASE_DIR, 'website', 'static'),
 ]
 
 # Default primary key field type
